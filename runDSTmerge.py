b47c80d87f9398ca3a9f187d0d41994c249ae997
TiborILD <kurca@in2p3.fr>
2018-07-20 15:31:12 +0200

    scripts to create/submit DST-merging jobs

---
+++ runDSTmerge.py
@@ -0,0 +1,174 @@
+from DIRAC.Core.Base import Script
+Script.parseCommandLine()
+
+from ILCDIRAC.Interfaces.API.DiracILC import DiracILC
+from ILCDIRAC.Interfaces.API.NewInterface.UserJob import UserJob
+from ILCDIRAC.Interfaces.API.NewInterface.Applications import Marlin, OverlayInput, DDSim, SLCIOConcatenate
+from ILCDIRAC.Interfaces.API.NewInterface.LCUtilityApplication import LCUtilityApplication
+import sys, time 
+from get_nevts_inIx import *
+from time import gmtime, strftime
+
+#global mergeList
+
+energy = 250
+ILDConfigVer  = "v02-00-01"
+ILCSoftVer    = "ILCSoft-02-00-01_gcc49"
+detectorModel = "ILD_l5_o2_v02"
+detModelSim   = "ILD_l5_v02"
+#outpath="grid/MyTest/mrg"
+
+inChannel=str($idChannel)
+ireq="I" + inChannel
+iFile=$ixnfile
+mrgx1=$mrg1
+mrgx2=$mrg2
+mergeList=$mrglist
+evtsPerMrg=$evtspmrg
+
+#print mergeList[0]
+#print mergeList
+
+
+if mrgx1 < 10:
+   mrgix1 = "00" + str(mrgx1)
+elif (mrgx1>9 and mrgx1 < 100)  :
+   mrgix1 = "0" + str(mrgx1)
+else  :
+   mrgix1 = str(mrgx1)
+
+if mrgx2 < 10:
+   mrgix2 = "00" + str(mrgx2)
+elif (mrgx2>9 and mrgx2 < 100)  :
+   mrgix2 = "0" + str(mrgx2)
+else  :
+   mrgix2 = str(mrgx2)
+
+mrg_range=mrgix1 + "-" + mrgix2
+
+
+if iFile < 10:
+   ix = ".00" + str(iFile)
+elif (iFile>9 and iFile < 100)  :
+   ix = ".0" +str(iFile)
+else  :
+   ix = "." + str(iFile)
+
+idin = ireq + ix 
+print "runDSTmerge:", idin, ireq, ix, mrg_range, evtsPerMrg
+if   ireq == "I106485" :
+   infile = "E" + str(energy) + "-TDR_ws.Pqqh.Gwhizard-1_95.eL.pR." + idin
+   chann ="higgs"
+elif   ireq == "I106486" :
+   infile = "E" + str(energy) + "-TDR_ws.Pqqh.Gwhizard-1_95.eR.pL." + idin
+   chann ="higgs"
+elif ireq == "I106551" :
+   infile = "E" + str(energy) + "-TDR_ws.P4f_ww_h.Gwhizard-1_95.eL.pR." + idin
+   chann ="WW_h"
+elif ireq == "I106552" :
+   infile = "E" + str(energy) + "-TDR_ws.P4f_ww_h.Gwhizard-1_95.eR.pL." + idin
+   chann ="WW_h"
+elif ireq == "I106559" :
+   infile = "E" + str(energy) + "-TDR_ws.P4f_sze_sl.Gwhizard-1_95.eL.pL." + idin
+   chann ="sZe_sl"
+elif ireq == "I106560" :
+   infile = "E" + str(energy) + "-TDR_ws.P4f_sze_sl.Gwhizard-1_95.eL.pR." + idin
+   chann ="sZe_sl"
+elif ireq == "I106561" :
+   infile = "E" + str(energy) + "-TDR_ws.P4f_sze_sl.Gwhizard-1_95.eR.pR." + idin
+   chann ="sZe_sl"
+elif ireq == "I106562" :
+   infile = "E" + str(energy) + "-TDR_ws.P4f_sze_sl.Gwhizard-1_95.eR.pL." + idin
+   chann ="sZe_sl"
+elif ireq == "I106563" :
+   infile = "E" + str(energy) + "-TDR_ws.P4f_sw_sl.Gwhizard-1_95.eL.pL." + idin
+   chann ="sW_sl"
+elif ireq == "I106564" :
+   infile = "E" + str(energy) + "-TDR_ws.P4f_sw_sl.Gwhizard-1_95.eL.pR." + idin
+   chann ="sW_sl"
+elif ireq == "I106565" :
+   infile = "E" + str(energy) + "-TDR_ws.P4f_sw_sl.Gwhizard-1_95.eR.pR." + idin
+   chann ="sW_sl"
+elif ireq == "I106566" :
+   infile = "E" + str(energy) + "-TDR_ws.P4f_sw_sl.Gwhizard-1_95.eR.pL." + idin
+   chann ="sW_sl"
+elif ireq == "I106571" :
+   infile = "E" + str(energy) + "-TDR_ws.P4f_sznu_sl.Gwhizard-1_95.eL.pR." + idin
+   chann ="sZnu_sl"
+elif ireq == "I106572" :
+   infile = "E" + str(energy) + "-TDR_ws.P4f_sznu_sl.Gwhizard-1_95.eR.pL." + idin
+   chann ="sZnu_sl"
+elif ireq == "I106573" :
+   infile = "E" + str(energy) + "-TDR_ws.P4f_zz_h.Gwhizard-1_95.eL.pR." + idin
+   chann ="ZZ_h"
+elif ireq == "I106574" :
+   infile = "E" + str(energy) + "-TDR_ws.P4f_zz_h.Gwhizard-1_95.eR.pL." + idin
+   chann ="ZZ_h"
+elif ireq == "I106575" :
+   infile = "E" + str(energy) + "-TDR_ws.P4f_zz_sl.Gwhizard-1_95.eL.pR." + idin
+   chann ="ZZ_sl"
+elif ireq == "I106576" :
+   infile = "E" + str(energy) + "-TDR_ws.P4f_zz_sl.Gwhizard-1_95.eR.pL." + idin
+   chann ="ZZ_sl"
+elif ireq == "I106577" :
+   infile = "E" + str(energy) + "-TDR_ws.P4f_ww_sl.Gwhizard-1_95.eL.pR." + idin
+   chann ="WW_sl"
+elif ireq == "I106578" :
+   infile = "E" + str(energy) + "-TDR_ws.P4f_ww_sl.Gwhizard-1_95.eR.pL." + idin
+   chann ="WW_sl"
+elif ireq == "I106607" :
+   infile = "E" + str(energy) + "-TDR_ws.P2f_z_h.Gwhizard-1_95.eL.pR." + idin
+   lcinputpath="LFN:/grid/ilc/prod/ilc/mc-dbd/generated/250-TDR_ws/2f/"
+   chann ="Z_h"
+elif ireq == "I106608" :
+   infile = "E" + str(energy) + "-TDR_ws.P2f_z_h.Gwhizard-1_95.eR.pL." + idin
+   lcinputpath="LFN:/ilc/prod/ilc/mc-dbd/generated/250-TDR_ws/2f/"
+   chann ="Z_h"
+
+else:
+     print "Wrong Channel Id !!!!" , inChannel
+
+jobGrName="group_" + chann
+
+mrgOutfile = "m" + ILDConfigVer + ".m" + detectorModel + ".LQGSP_BERT." + infile
+lcoutputMRG  = mrgOutfile + "_%s.DST.slcio"%(mrg_range)
+print "runDSTmerge:  lcoutputMRG= ", lcoutputMRG
+
+jobGroup = idin + "_" + detModelSim
+dirac = DiracILC(True,jobGroup+".rep")
+
+MRGoutput = []
+# outputs to be saved onto grid SE
+outpath="MyProd_" + ILDConfigVer + "/E250-TDR_ws/" + chann + "/" +ireq+ "/mrg"
+
+
+#jobname="m" + idin + "_" + str(mrg_range)
+jobname="m" + idin + "_" + str(mrgix1)
+print jobname
+job = UserJob()
+job.setName(jobname)
+job.setJobGroup(jobGrName)
+job.setILDConfig(ILDConfigVer)
+job.setCPUTime(6400)
+job.setInputSandbox(["runDSTmerge_Tmp.py"])
+job.setOutputSandbox( ["*.log","*.sh","*.py"] )
+job.setInputData(mergeList)
+#job.setInputData(mergeList[0])
+#job.setOutputData( lcoutputMRG, outpath, "CERN-SRM" )
+job.setOutputData( lcoutputMRG, outpath, "IN2P3-SRM" )
+job.dontPromptMe()
+
+slcioconcat = SLCIOConcatenate()
+slcioconcat.setInputFile(mergeList)
+#slcioconcat.setInputFile(mergeList[0])
+#slcioconcat.setNumberOfEvents(4000)
+slcioconcat.setNumberOfEvents(evtsPerMrg)
+slcioconcat.setOutputFile( lcoutputMRG, outpath)
+slcioconcat.setLogFile("merge.log")
+
+mergejob = job.append(slcioconcat)
+if not mergejob['OK']:
+       print mergejob['Not ok appending slcioconcat to job']
+       quit()
+
+print job.submit(dirac)
---
+++ submitDSTmerge.py
@@ -0,0 +1,164 @@
+from DIRAC.Core.Base import Script
+Script.parseCommandLine()
+from ILCDIRAC.Interfaces.API.NewInterface.UserJob import UserJob
+from ILCDIRAC.Interfaces.API.NewInterface.Applications import Marlin, OverlayInput, DDSim, SLCIOConcatenate
+from ILCDIRAC.Interfaces.API.NewInterface.LCUtilityApplication import LCUtilityApplication
+from ILCDIRAC.Interfaces.API.DiracILC import DiracILC
+import os, sys, time 
+import subprocess, commands
+from get_nevts_inIx import *
+from time import gmtime, strftime
+
+fR = file("runDSTmerge.py","r")
+lines = fR.read()
+fR.close()
+
+
+def submitDSTmerge(inChannel):
+
+   global lines, mergeList
+
+#   prefixpth="LFN:/ilc/user/k/kurca/grid/ilc/prod/ilc/mc-dbd/ild/MyProd_v02-00/E250-TDR_ws/"
+   inFilesList=[]
+   ireq="I" + str(inChannel)
+   nEvtsArray=get_nevts_inIx(inChannel)
+   nfiles= len(nEvtsArray)
+   strinChannel= str(inChannel)
+   evtsPerRun=200
+   nfmerge = 50 
+   evtsPmerge = evtsPerRun*nfmerge
+
+   text="No such file or directory"
+#   text="Unknown"
+#   evts=nEvtsArray[iFile]
+#   nJobs = nEvtsArray/evtsPerRun
+#   if (nevts%evtsPerRun >9):
+#       nJobs = nJobs +1
+#   nfmrg_rest=nJobs%nfmerge
+#   print "nfmrg_rest: ", nfmrg_rest
+
+
+   print "submitDSTmerge: nfiles ",nfiles, "inChannel=", inChannel
+   if inChannel == '106485':
+       lfnlist = file("ilc-user-k-kurca-MyProd_v02-00-01-E250-TDR_ws-higgs-I106485-rec-DST.lfns","r")
+   elif inChannel == '106486':
+       lfnlist = file("ilc-user-k-kurca-MyProd_v02-00-01-E250-TDR_ws-higgs-I106486-rec-DST.lfns","r")
+   elif inChannel == '106551':
+       lfnlist = file("ilc-user-k-kurca-MyProd_v02-00-01-E250-TDR_ws-WW_h-I106551-rec-DST.lfns","r")
+   elif inChannel == '106552':
+       lfnlist = file("ilc-user-k-kurca-MyProd_v02-00-01-E250-TDR_ws-WW_h-I106552-rec-DST.lfns","r")
+   elif inChannel == '106559':
+       lfnlist = file("ilc-user-k-kurca-MyProd_v02-00-01-E250-TDR_ws-sZe_sl-I106559-rec-DST.lfns","r")
+   elif inChannel == '106560':
+       lfnlist = file("ilc-user-k-kurca-MyProd_v02-00-01-E250-TDR_ws-sZe_sl-I106560-rec-DST.lfns","r")
+       #lfnlist = file("short_I106560-rec-DST.lfns","r")
+   elif inChannel == '106561':
+       lfnlist = file("ilc-user-k-kurca-MyProd_v02-00-01-E250-TDR_ws-sZe_sl-I106561-rec-DST.lfns","r")
+   elif inChannel == '106562':
+       lfnlist = file("ilc-user-k-kurca-MyProd_v02-00-01-E250-TDR_ws-sZe_sl-I106562-rec-DST.lfns","r")
+   elif inChannel == '106563':
+       lfnlist = file("ilc-user-k-kurca-MyProd_v02-00-01-E250-TDR_ws-sW_sl-I106563-rec-DST.lfns","r")
+   elif inChannel == '106564':
+       lfnlist = file("ilc-user-k-kurca-MyProd_v02-00-01-E250-TDR_ws-sW_sl-I106564-rec-DST.lfns","r")
+   elif inChannel == '106565':
+       lfnlist = file("ilc-user-k-kurca-MyProd_v02-00-01-E250-TDR_ws-sW_sl-I106565-rec-DST.lfns","r")
+   elif inChannel == '106566':
+       lfnlist = file("ilc-user-k-kurca-MyProd_v02-00-01-E250-TDR_ws-sW_sl-I106566-rec-DST.lfns","r")
+   elif inChannel == '106571':
+       lfnlist = file("ilc-user-k-kurca-MyProd_v02-00-01-E250-TDR_ws-sZnu_sl-I106571-rec-DST.lfns","r")
+   elif inChannel == '106572':
+       lfnlist = file("ilc-user-k-kurca-MyProd_v02-00-01-E250-TDR_ws-sZnu_sl-I106572-rec-DST.lfns","r")
+   elif inChannel == '106573':
+       lfnlist = file("ilc-user-k-kurca-MyProd_v02-00-01-E250-TDR_ws-ZZ_h-I106573-rec-DST.lfns","r")
+   elif inChannel == '106574':
+       lfnlist = file("ilc-user-k-kurca-MyProd_v02-00-01-E250-TDR_ws-ZZ_h-I106574-rec-DST.lfns","r")
+   elif inChannel == '106575':
+       lfnlist = file("ilc-user-k-kurca-MyProd_v02-00-01-E250-TDR_ws-ZZ_sl-I106575-rec-DST.lfns","r")
+   elif inChannel == '106607':
+       lfnlist = file("ilc-user-k-kurca-MyProd_v02-00-01-E250-TDR_ws-Z_h-I106607-rec-DST.lfns","r")
+   elif inChannel == '106608':
+       lfnlist = file("ilc-user-k-kurca-MyProd_v02-00-01-E250-TDR_ws-Z_h-I106608-rec-DST.lfns","r")
+   else :
+        "Wrong Channel !!!! "
+
+#create input list of files to be merged ?!!!  only from existing files! ..... readin a list !!!
+   allFilesList = []
+   for line in lfnlist:
+        allFilesList.append(line)
+   lfnlist.close()
+#   xfiles = 1 
+#   for iFile in range(xfiles,xfiles+1,1):
+   for iFile in range(2,nfiles,1):
+       if iFile < 10:
+          ix = ".00" + str(iFile)
+       elif (iFile>9 and iFile < 100)  :
+          ix = ".0" +str(iFile)
+       else  :
+          ix = "." + str(iFile)
+    
+       print str(ix) 
+       idin = "I"+ str(inChannel) +  ix
+
+       inFilesList = "inFilesList" + ix
+       inFilesList = []
+       for exprs in allFilesList:
+            if idin in exprs:
+#               exprs="LFN:" + exprs
+               l1=exprs.strip()
+               inFilesList.append(l1)
+#               print exprs 
+#               print l1
+#       print idin,inFilesList, len(inFilesList)
+#   print(inFilesList[0:10])
+# create list of lists (with 10 files to be merged)
+       ndstfiles = len(inFilesList) 
+       print ndstfiles 
+       for i in range (0,ndstfiles,nfmerge):
+#       for i in range (100,ndstfiles,nfmerge):
+#       for i in range (150,180,nfmerge):
+#        print(i, inFilesList[i:i+nfmerge])
+          m1=str(i+1)
+          m2=str(i+nfmerge)
+          print "Loopi:",i,ndstfiles 
+          if (ndstfiles - i) < nfmerge :
+            m2=str(ndstfiles)
+            print(i,m2,ndstfiles )
+          mrgrange = m1 + "-" + m2 
+          xmergeList = "mergeList_" +idin + "_" + mrgrange 
+          xmergeList = []
+          xmergeList.append(inFilesList[i:i+nfmerge])
+          print i, mrgrange
+          xnfmerge = 0 
+          mergeList = []
+          # check the existence of file before adding to the final list of files to be merged
+          for ifile in xmergeList[0]:
+              status,output = commands.getstatusoutput("dirac-dms-lfn-replicas %s" %ifile)
+#              print status, output
+              if not text in output:
+                 mergeList.append(ifile)
+                 xnfmerge = xnfmerge +1
+          evtsPmerge = evtsPerRun*xnfmerge
+#          print "mergeList:", i, mrgrange, xnfmerge
+          print "mergeList:", i, mrgrange, xnfmerge, mergeList
+          t= Template(lines)
+          if os.path.exists("runDSTmerge_Tmp.py"):
+             os.remove("runDSTmerge_Tmp.py")
+          fT = file("runDSTmerge_Tmp.py","w")
+          fT.write(t.substitute(idChannel=strinChannel,mrg1=m1,mrg2=m2,mrglist=str(mergeList),ixnfile=str(iFile),evtspmrg=str(evtsPmerge)))
+          fT.close()
+          fT1 = file("runDSTmerge_"+idin + "_" + m1 + ".py","w")
+          fT1.write(t.substitute(idChannel=strinChannel,mrg1=m1,mrg2=m2,mrglist=str(mergeList),ixnfile=str(iFile),evtspmrg=str(evtsPmerge)))
+          fT1.close()
+          f2 = subprocess.call("python runDSTmerge_Tmp.py", shell=True)
+
+#       print strftime("%Y-%m-%d %H:%M:%S", gmtime())
+#       time.sleep(1)   # Delay for 10 seconds
+#       print strftime("%Y-%m-%d %H:%M:%S", gmtime())
+
+if __name__ == "__main__":
+  if len(sys.argv) !=2:
+    print "usage: python submitDSTmerge (e.g.:  106653)"
+    print sys.argv[1]
+    sys.exit(-1)
+  submitDSTmerge(sys.argv[1])
+
